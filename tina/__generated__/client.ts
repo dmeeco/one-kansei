69c7017dc54a267701f78f971d58bb6c45617ff9
DK <50326711+dmeeco@users.noreply.github.com>
2025-07-18 14:56:28 +0200

    Fix purgecss dependency resolution for Cloudflare Pages

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Users/humanx/Documents/11straps-1.0/tina/__generated__/.cache/1752834634843', url: 'https://content.tinajs.io/1.6/content/your_client_ID_here/github/master', token: 'your_content_readonly_token_here', queries,  });
+export const client = createClient({ cacheDir: '/Users/humanx/Documents/11straps-1.0/tina/__generated__/.cache/1752843349776', url: 'https://content.tinajs.io/1.6/content/your_client_ID_here/github/master', token: 'your_content_readonly_token_here', queries,  });
 export default client;
   
