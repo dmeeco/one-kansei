b12b293985a7180e31a835701e03bf1a2bfa6977
DK <50326711+dmeeco@users.noreply.github.com>
2025-08-03 15:25:57 +0200

    Fix misc

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Users/humanx/Documents/11straps-1.0/tina/__generated__/.cache/1754204693972', url: 'https://content.tinajs.io/1.6/content/your_client_ID_here/github/master', token: 'your_content_readonly_token_here', queries,  });
+export const client = createClient({ cacheDir: '/Users/humanx/Documents/11straps-1.0/tina/__generated__/.cache/1754227528435', url: 'https://content.tinajs.io/1.6/content/your_client_ID_here/github/master', token: 'your_content_readonly_token_here', queries,  });
 export default client;
   
